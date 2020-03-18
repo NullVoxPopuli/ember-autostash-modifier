133289f7f140c30897bc3b9934b71884bd4a6b20
NullVoxPopuli <LPSego3+dev@gmail.com>
2020-03-18 17:19:42 -0400

    yeah, this is good

--- addon/modifiers/autostash.ts
+++ addon/modifiers/autostash.ts
@@ -1,74 +1,83 @@
 import Modifier from 'ember-modifier';
-import { get, set, action } from '@ember/object';
+import { action } from '@ember/object';
 import { guidFor } from '@ember/object/internals';
 import { inject as service } from '@ember/service';
 
-import AutostashStore from 'autostash/services/autostash/store';
+type AutostashStore =  import('autostash/services/autostash/store');
 
-type StashableElement = HTMLInputElement | HTMLTextAreaElement;
+type StashableElement =
+  | HTMLInputElement
+  | HTMLTextAreaElement;
+
 type Key = string | number;
+type GetValue = () => string | boolean;
+type SetValue = (value: string | boolean) => void;
 
 interface Args {
   positional: [Key],
   named: {};
 }
 
-const LS_KEY = '__AutoStash__';
-
 export default class Autostash extends Modifier<Args> {
   @service('autostash/store') store!: AutostashStore;
 
-  lastWhen?: Key;
+  lastKey?: string;
+  lastValue?: string | boolean;
+
+  getValue: GetValue = () => (this.element as StashableElement).value;
+  setValue: SetValue = (value: string) => (this.element as StashableElement).value = value;
 
   didInstall() {
-    this.lastWhen = guidFor(this.args.positional[0]);
+    this.lastKey = keyFor(this.args.positional[0], this.element);
 
     this.element?.addEventListener('input', this.didReceiveArguments)
+
+    if (isCheckbox(this.element)) {
+      this.getValue = () => (this.element as HTMLInputElement).checked;
+      this.setValue = (value: boolean) => (this.element as HTMLInputElement).checked = value;
+    }
   }
 
   @action
   didReceiveArguments() {
-    if (!this.lastWhen) return;
+    if (!this.lastKey) return;
     if (!this.element) return;
-    if (!hasValue(this.element)) return;
+    if (this.lastValue === this.getValue()) return;
 
-    let when = guidFor(this.args.positional[0]);
+    let key = keyFor(this.args.positional[0], this.element);
 
-    if (when === this.lastWhen) {
-      let data = this.element.value;
-
-      this.store.record(when, data);
+    if (key === this.lastKey) {
+      this.record(key);
       return;
     }
 
-    // this.element.value = lookup(when);
-    this.element.value = this.store.lookup(when) || '';
-
-    this.element?.dispatchEvent(new Event('input', { bubbles: true, cancelable: true }));
-
-    this.lastWhen = when;
+    this.restore(key);
   }
 
   willRemove() {
     this.element?.removeEventListener('input', this.didReceiveArguments);
   }
+
+  record(key: string) {
+    let data = this.getValue();
+
+    this.store.record(key, data);
+  }
+
+  restore(key: string) {
+    this.setValue(this.store.lookup(key) || '');
+
+    this.lastKey = key;
+    this.element?.dispatchEvent(new Event('input', { bubbles: true, cancelable: true }));
+  }
 }
 
-// function set(key: Key, value: string) {
-//   let lsKey = `${LS_KEY}${key}`;
+function keyFor(key: string | number, element?: Element | null) {
+  return `__${guidFor(key)}-${guidFor(element)}__`;
+}
 
-//   window.localStorage.setItem(lsKey, value);
-// }
+function isCheckbox(element?: Element | null): element is HTMLInputElement {
+  if (!element) return false;
 
-// function lookup(key: Key) {
-//   let lsKey = `${LS_KEY}${key}`;
-
-//   return window.localStorage.getItem(lsKey) || '';
-// }
-
-
-function hasValue(element: Element): element is StashableElement {
-  if ('value' in element) return true;
-
-  return false;
+  return element.getAttribute('type') === 'checkbox';
 }
--- tests/integration/stashing-test.ts
+++ tests/integration/stashing-test.ts
@@ -1,12 +1,12 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render, fillIn, find, settled } from '@ember/test-helpers';
+import { render, fillIn, find, settled, click } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 
 module('{{autostash}}', function(hooks) {
   setupRenderingTest(hooks);
 
-  module('<input>', function() {
+  module('<input {{autostash this.key}}>', function() {
     test('it works', async function(assert) {
       this.setProperties({ key: 1 });
 
@@ -30,9 +30,53 @@ module('{{autostash}}', function(hooks) {
     });
   });
 
-  module('<textarea>', function() {
+  module('<textarea  {{autostash this.key}}>', function() {
     test('it works', async function(assert) {
+      this.setProperties({ key: 1 });
 
+      await render(hbs`
+        <textarea data-test-input {{autostash this.key}} />
+      `);
+
+      await fillIn('[data-test-input]', 'one');
+
+      let input = find('[data-test-input]') as HTMLInputElement;
+
+      assert.equal(input.value, 'one');
+
+      this.setProperties({ key: 2 });
+      await settled();
+
+      assert.equal(input.value, '');
+
+      this.setProperties({ key: 1 });
+      await settled();
+
+      assert.equal(input.value, 'one');
+    });
+  });
+
+  module('<input type="checkbox {{autostash this.key}}>', function() {
+    test('it works', async function(assert) {
+      this.setProperties({ key: 1 });
+
+      await render(hbs`
+        <input type='checkbox' data-test-input {{autostash this.key}} />
+      `);
+
+      await click('[data-test-input]');
+
+      assert.dom('[data-test-input]').isChecked();
+
+      this.setProperties({ key: 2 });
+      await settled();
+
+      assert.dom('[data-test-input]').isNotChecked();
+
+      this.setProperties({ key: 1 });
+      await settled();
+
+      assert.dom('[data-test-input]').isChecked();
     });
   });
 });
