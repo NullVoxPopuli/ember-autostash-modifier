a7ffd001740a438f0531522844b34babce5a25a2
NullVoxPopuli <LPSego3+dev@gmail.com>
2021-12-19 20:54:49 -0500

    chore(support): drop ember 3.16, support 3.24+

--- config/ember-try.js
+++ config/ember-try.js
@@ -7,10 +7,18 @@ module.exports = async function() {
     useYarn: true,
     scenarios: [
       {
-        name: 'ember-lts-3.16',
+        name: 'ember-lts-3.24',
         npm: {
           devDependencies: {
-            'ember-source': '~3.16.0'
+            'ember-source': '~3.24.0'
+          }
+        }
+      },
+      {
+        name: 'ember-lts-3.28',
+        npm: {
+          devDependencies: {
+            'ember-source': '~3.28.0'
           }
         }
       },
